e97c2bfc0b872383887ccef7b1fb874504c05fea
Marcelo Burgos Morgade Cortizo <morgade@gmail.com>
2019-10-02 10:10:11 -0300

    PermCheck

---
+++ src/main/java/com/morgade/codility/challenges/PermCheck.java
@@ -0,0 +1,30 @@
+package com.morgade.codility.challenges;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class PermCheck {
+
+    public static void main(String[] args) {
+        SolutionPermCheck s = new SolutionPermCheck();
+        System.out.println(s.solution(new int[] {4,1,3}));
+    }
+}
+
+class SolutionPermCheck {
+
+    public int solution(int[] A) {
+        boolean[] found = new boolean[A.length];
+        
+        for (int i = 0; i < found.length; i++) {
+            if (A[i]>A.length || found[A[i]-1]) {
+                return 0;
+            } else {
+                found[A[i]-1] = true;
+            }
+        }
+        
+        return 1;
+    }
+}
